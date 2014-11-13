183d191724b636824e9b9c27a075264e7e6414c0
Robert Vock <robert.vock@4wdmedia.de>
2014-11-13 17:56:28 +0100

    Override check for case-sensitive file-system

---
+++ Classes/Resources/StorageRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Vierwd\VierwdBase\Resource;
+
+/**
+ * Override check for case-sensitivity.
+ * We develop on OS X, which has a non-case-sensitive file system. But when we upload
+ * the project to a Linux-server, the file-system is case-sensitive, so it must be
+ * enabled on our machines as well
+ */
+class StorageRepository extends \TYPO3\CMS\Core\Resource\StorageRepository {
+
+	protected function testCaseSensitivity($absolutePath) {
+		return true;
+	}
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,3 +3,7 @@
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['EBT\\ExtensionBuilder\\Service\\FileGenerator'] = array(
 	'className' => 'Vierwd\\VierwdBase\\ExtensionBuilder\\Service\\FileGenerator',
 );
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Resource\\StorageRepository'] = array(
+	'className' => 'Vierwd\\VierwdBase\\Resource\\StorageRepository',
+);
