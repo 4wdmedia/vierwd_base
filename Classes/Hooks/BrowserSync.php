9d306dbf0b71a1a5302afaf25d2d2f7cab49fd3b
Robert Vock <robert.vock@4wdmedia.de>
2014-12-10 17:34:45 +0100

    moved Browser Sync to Base-Class

---
+++ Classes/Hooks/BrowserSync.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vierwd\VierwdBase\Hooks;
+
+class BrowserSync {
+	public function enable() {
+		if (empty($_SERVER['4WD_CONFIG'])) {
+			return '';
+		}
+
+		// check if the port 3000 is open
+		if (!trim(`lsof -i :3000 -P | grep "^node.*3000"`)) {
+			return '';
+		}
+
+		return '<script async src="http://' . $_SERVER['SERVER_NAME'] . ':3000/browser-sync-client.js"></script>';
+	}
+}
