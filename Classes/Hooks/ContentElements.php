cdf80969836ca57f20f30d3ae41a91f81fb4285b
Robert Vock <robert.vock@4wdmedia.de>
2014-12-11 09:55:27 +0100

    moved content-elements Class

---
+++ Classes/Hooks/ContentElements.php
@@ -0,0 +1,293 @@
+<?php
+
+namespace Vierwd\VierwdBase\Hooks;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Robert Vock <robert.vock@4wdmedia.de>, 4wd media
+ *
+ *  All rights reserved
+ *
+ ***************************************************************/
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\PathUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
+/**
+ * @package vierwd_base
+ */
+class ContentElements {
+
+	public static $oldProcFunc;
+
+	/**
+	 * process the CType and sort custom FCEs into a special group
+	 */
+	public function process_CType($params, $refObj) {
+		if (static::$oldProcFunc) {
+			GeneralUtility::callUserFunction(static::$oldProcFunc, $params, $refObj);
+		}
+
+		$params['items'][] = array('{project.name}', '--div--');
+		$appendKeys = array();
+		$append = array();
+		foreach ($params['items'] as $key => $data) {
+			if (substr($data[1], 0, strlen('{project.extensionName}_')) == '{project.extensionName}_') {
+				$appendKeys[] = $key;
+				$append[] = $data;
+			}
+		}
+
+		foreach ($appendKeys as $key) {
+			unset($params['items'][$key]);
+		}
+
+		usort($append, function($plugin1, $plugin2) {
+			return strnatcasecmp($plugin1[0], $plugin2[0]);
+		});
+
+		foreach ($append as $data) {
+			$params['items'][] = $data;
+		}
+
+		return $params['items'];
+	}
+
+	/**
+	 * add Content Elements
+	 *
+	 * @param string $extensionKey
+	 */
+	static public function addFCEs($extensionKey, $isLocalConf = false) {
+		global $TCA;
+
+		$fceDir = ExtensionManagementUtility::extPath($extensionKey) . 'Configuration/FCE/';
+
+		$pageTS = '';
+		$typoScript = '';
+
+		$defaults = include $fceDir . '_defaults.php';
+
+		foreach (new \DirectoryIterator($fceDir) as $fceConfigFile) {
+			if ($fceConfigFile->isDot() || $fceConfigFile->isDir() || substr($fceConfigFile->getFilename(), 0, 1) == '_') {
+				continue;
+			}
+
+			if (substr($fceConfigFile->getFilename(), -4) != '.php') {
+				continue;
+			}
+
+			$config = include $fceConfigFile->getPathname();
+			$config = $config + $defaults;
+
+			if (!$isLocalConf) {
+				// ext_tables
+
+				$name = $config['name'];
+				if (empty($name) && !empty($config['list_type'])) {
+					foreach ($TCA['tt_content']['columns']['list_type']['config']['items'] as $plugin) {
+						if ($plugin[1] == $config['list_type']) {
+							$name = $plugin[0];
+							break;
+						}
+					}
+				}
+
+				if (!$name) {
+					throw new \Exception('Missing FCE name');
+				}
+
+				ExtensionManagementUtility::addPlugin(array($name, $config['CType']), 'CType', $extensionKey);
+
+				if ($config['flexform']) {
+					ExtensionManagementUtility::addPiFlexFormValue('*', 'FILE:EXT:' . $extensionKey . '/Configuration/FlexForms/' . $config['flexform'], $config['CType']);
+				}
+
+				$pageTS .=
+					'mod.wizards.newContentElement.wizardItems.vierwd.elements.' . $config['CType'] . ' {' . "\n" .
+					'	icon = ' . $config['icon'] . "\n" .
+					'	title = ' . $name . "\n" .
+					'	description = ' . $config['description'] . "\n" .
+					'	tt_content_defValues {' . "\n" .
+					'		CType = ' . $config['CType'] . "\n" .
+					'	}' . "\n" .
+					'}' . "\n";
+
+				$tca = $config['fullTCA'] ? $config['fullTCA'] : self::generateTCA($config);
+
+				$TCA['tt_content']['types'][$config['CType']]['showitem'] = $tca;
+			}
+
+			// update typoscript
+			if ($config['list_type']) {
+				$typoScript .= 'tt_content.' . $config['CType'] . ' < tt_content.list.20.'.$config['list_type']. "\n";
+			}
+			foreach ($config['switchableControllerActions'] as $controller => $actions) {
+				$i = 1;
+
+				if (!is_array($actions)) {
+					$actions = GeneralUtility::trimExplode(',', $actions, true);
+				}
+
+				foreach ($actions as $action) {
+					$typoScript .= 'tt_content.' . $config['CType'] . '.switchableControllerActions.' . $controller . '.' . $i++ . ' = ' . $action . "\n";
+				}
+			}
+		}
+
+		if ($typoScript && $isLocalConf) {
+			ExtensionManagementUtility::addTypoScript($extensionKey, 'setup', $typoScript, 'defaultContentRendering');
+		}
+
+		if ($pageTS && !$isLocalConf) {
+			ExtensionManagementUtility::addPageTSConfig($pageTS);
+		}
+	}
+
+	public function generateTCA(array $config) {
+		$tcaType = explode(',', $config['tcaType']);
+
+		// bodytext,richtext,image,fullheaders
+		if (in_array('fullheaders', $tcaType)) {
+			$header = '--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.headers;headers,';
+		} else if (in_array('simpleheaders', $tcaType)) {
+			$header = '--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.header;header,';
+		} else {
+			$header = 'header;LLL:EXT:cms/locallang_ttc.xlf:header.ALT.div_formlabel,';
+		}
+
+		$bodytext = '';
+		if (in_array('bodytext', $tcaType) || in_array('richtext', $tcaType)) {
+			$bodytext = 'bodytext;LLL:EXT:cms/locallang_ttc.xml:bodytext_formlabel';
+
+			if (in_array('richtext', $tcaType)) {
+				$bodytext .= ';;richtext:rte_transform[flag=rte_enabled|mode=ts_css]';
+			}
+
+			$bodytext .= ',';
+		}
+
+		if (in_array('image', $tcaType)) {
+			$image = '--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.images,
+				image,
+				--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.imagelinks;imagelinks,';
+		} else {
+			$image = '';
+		}
+		if ($config['flexform']) {
+			$flexform = 'pi_flexform,';
+		} else {
+			$flexform = '';
+		}
+
+		$tca = '
+				--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.general;general,
+				' . $header . '
+				' . $bodytext . '
+				' . $flexform . '
+				' . $image . '
+			--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.appearance,
+				--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.frames;frames,
+			--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,
+				--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.visibility;visibility,
+				--palette--;LLL:EXT:cms/locallang_ttc.xml:palette.access;access,
+			--div--;LLL:EXT:cms/locallang_ttc.xml:tabs.extended';
+
+		return $tca;
+	}
+
+	public function getColumnWidth($data, $column, $size, $zeroOnHidden = true) {
+		if ($zeroOnHidden && strpos($data['flexform_visibility_col' . $column], 'hidden-' . $size) !== false) {
+			// column not visible
+			return 0;
+		}
+
+		if ($data['flexform_width_column_' . $size . '_' . $column]) {
+			return substr($data['flexform_width_column_' . $size . '_' . $column], 7);
+		} else {
+			// check width of lower columns
+			switch ($size) {
+				case 'lg':
+					return $this->getColumnWidth($data, $column, 'md', false);
+				case 'md':
+					return $this->getColumnWidth($data, $column, 'sm', false);
+				case 'sm':
+					return $this->getColumnWidth($data, $column, 'xs', false);
+				default:
+					return 12;
+			}
+		}
+	}
+
+	public function resetGrid($content) {
+		$this->clearfix = null;
+		return $content;
+	}
+
+	public function calculateClearfix($data, $columns) {
+		$widths = array(
+			'xs' => 0,
+			'sm' => 0,
+			'md' => 0,
+			'lg' => 0,
+		);
+		$clearfix = array(
+			'xs' => array(),
+			'sm' => array(),
+			'md' => array(),
+			'lg' => array(),
+		);
+
+		foreach (array_keys($widths) as $size) {
+			for ($i=1; $i < $columns; $i++) {
+				$width = $this->getColumnWidth($data, $i, $size);
+				if (!$width) {
+					continue;
+				}
+				if ($width == 12) {
+					// no clearfix needed for width of 12
+					$widths[$size] = 0;
+					continue;
+				}
+				$widths[$size] += $width;
+				if ($widths[$size] >= 12) {
+					$clearfix[$size][$i] = true;
+					$widths[$size] = 0;
+				} else {
+					// check if it would be larger with the next column
+					if ($widths[$size] + $this->getColumnWidth($data, $i + 1, $size) > 12) {
+						$clearfix[$size][$i] = true;
+						$widths[$size] = 0;
+					}
+				}
+			}
+		}
+
+		$this->clearfix = $clearfix;
+	}
+
+	public function clearfixGrid($content, $params) {
+		$data = $this->cObj->data;
+		$column = $params['column'];
+		$columns = $params['columns'];
+
+		if (!$this->clearfix) {
+			$this->calculateClearfix($data, $columns);
+		}
+
+		$clear = '';
+
+		foreach ($this->clearfix as $size => $clearfixes) {
+			foreach ($clearfixes as $clearAfterColumn => $true) {
+				if ($clearAfterColumn == $column) {
+					$clear .= '<div class="clearfix visible-' . $size . '"></div>';
+				}
+			}
+		}
+
+		return $clear;
+	}
+}
