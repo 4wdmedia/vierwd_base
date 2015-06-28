e97c7ac3e68fa054a4aca3254ebfac301f6bf477
Robert Vock <robertvock82@gmail.com>
2015-06-28 11:19:10 +0200

    disable csh and felogin during installation

---
+++ Classes/Install/Controller/Action/Step/DatabaseData.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Vierwd\VierwdBase\Install\Controller\Action\Step;
+
+class DatabaseData extends \TYPO3\CMS\Install\Controller\Action\Step\DatabaseData {
+
+	/**
+	 * make sure felogin and context_help are not active.
+	 * Both are part of the default-system, which is specified in their composer.json (partOfFactoryDefault).
+	 * It is not possible to change this setting and it is not possible to overwrite the Package or PackageManager
+	 * to change this setting.
+	 * This is why we override a Step during the installation which happens AFTER those packages have been activated.
+	 */
+	public function execute() {
+		$packageManager = \TYPO3\CMS\Core\Core\Bootstrap::getInstance()->getEarlyInstance('TYPO3\\Flow\\Package\\PackageManager');
+		$packageManager->deactivatePackage('context_help');
+		$packageManager->deactivatePackage('felogin');
+
+		return parent::execute();
+	}
+}
