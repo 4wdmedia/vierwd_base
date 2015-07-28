c91d3d975398461a03e9a5260bfd63a3d2353d70
Robert Vock <robert.vock@4wdmedia.de>
2015-07-28 17:12:52 +0200

    moved TCA override to base-Extension, set header to required

---
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -0,0 +1,4 @@
+<?php
+
+// Disable file Upload in content-elements
+$GLOBALS['TCA']['sys_file_reference']['columns']['uid_local']['config']['appearance']['fileUploadAllowed'] = false;
---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,4 @@
+<?php
+
+// Header is a required field
+$GLOBALS['TCA']['tt_content']['columns']['header']['config']['eval'] = 'trim,required';
