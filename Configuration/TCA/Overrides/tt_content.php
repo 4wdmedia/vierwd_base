ca2f8244560da851c0a1563264be47a54a87172a
Robert Vock <robert.vock@4wdmedia.de>
2015-08-11 14:26:59 +0200

    added TypoScript wizard for TypoScript content element

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,3 +2,22 @@
 
 // Header is a required field
 $GLOBALS['TCA']['tt_content']['columns']['header']['config']['eval'] = 'trim,required';
+
+// add TypoScript wizard
+$wizards =& $GLOBALS['TCA']['tt_content']['columns']['bodytext']['config']['wizards'];
+$wizards['t3editorTypoScript'] = [
+	'enableByTypeConfig' => '1',
+	'icon' => 'wizard_table.gif',
+	'module' => [
+		'name' => 'wizard_table',
+	],
+	'params' => [
+		'format' => 'ts',
+		'style' => 'width:98%; height: 60%;',
+	],
+	'title' => 't3editor',
+	'type' => 'userFunc',
+	'userFunc' => 'TYPO3\CMS\T3editor\FormWizard->main'
+];
+
+unset($wizards);
