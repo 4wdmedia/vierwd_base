3720751810b0eb149c6a53bc2dfed11aba85d3dd
Robert Vock <robert.vock@4wdmedia.de>
2015-08-11 14:25:42 +0200

    Added error handler for Fatal errors

--- Classes/Error/ErrorHandler.php
+++ Classes/Error/ErrorHandler.php
@@ -4,19 +4,32 @@ namespace Vierwd\VierwdBase\Error;
 
 /**
  * error handler which uses Growl to show errors during developement.
+ * It also logs fatal errors
  *
- * Add with $TYPO3_CONF_VARS['SYS']['errorHandler'] = 'Vierwd\\VierwdBase\\Errors\\ErrorHandler';
+ * Add with $TYPO3_CONF_VARS['SYS']['errorHandler'] = 'Vierwd\\VierwdBase\\Error\\ErrorHandler';
  *
  * @note growl must be accessible in your path
  */
 class ErrorHandler extends \TYPO3\CMS\Core\Error\ErrorHandler {
+
+	public function __construct($errorHandlerErrors) {
+		parent::__construct($errorHandlerErrors);
+
+		register_shutdown_function(function() {
+			$error = error_get_last();
+			if ($error['type'] & E_ERROR) {
+				// Convert type to E_USER_ERROR, because TYPO3 doesn't know about E_ERROR
+				$this->handleError(E_USER_ERROR, $error['message'], $error['file'], $error['line']);
+			}
+		});
+	}
 
 	protected function writeLog($logMessage, $severity) {
 		static $messages = array();
 
 		parent::writeLog($logMessage, $severity);
 
-		if (in_array($logMessage, $messages)) {
+		if (empty($_SERVER['4WD_CONFIG']) || in_array($logMessage, $messages)) {
 			return;
 		}
 		$messages[] = $logMessage;
@@ -31,7 +44,7 @@ class ErrorHandler extends \TYPO3\CMS\Core\Error\ErrorHandler {
 		// check if it's possible to show notification with growl
 		$growl = trim(`which growl`);
 		if ($growl) {
-			shell_exec($growl . ' -nosticky ' . escapeshellarg($logMessage));
+			shell_exec($growl . ' -nosticky '.escapeshellarg($logMessage));
 		}
 	}
 }
