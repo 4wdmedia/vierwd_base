ad12efb1c79a76ce3d67b89233cfe2e523f1c5b1
Robert Vock <robert.vock@4wdmedia.de>
2016-02-11 11:17:32 +0100

    fixed formatting of generated classes

--- Classes/ExtensionBuilder/Service/Printer.php
+++ Classes/ExtensionBuilder/Service/Printer.php
@@ -2,6 +2,13 @@
 
 namespace Vierwd\VierwdBase\ExtensionBuilder\Service;
 
+use \PhpParser\Node\Stmt;
+
 class Printer extends \EBT\ExtensionBuilder\Service\Printer {
 	protected $indentToken = "\t";
+
+	public function pStmt_ClassMethod(Stmt\ClassMethod $node) {
+		$string = parent::pStmt_ClassMethod($node);
+		return str_replace(LF . '{', ' {', $string);
+	}
 }
