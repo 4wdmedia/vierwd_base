0ec1fd3740c017b2ab31149a085d9debd43f9652
Robert Vock <robert.vock@4wdmedia.de>
2016-03-15 15:41:59 +0100

    pretty class-statement for code generated by extension builder

--- Classes/ExtensionBuilder/Service/Printer.php
+++ Classes/ExtensionBuilder/Service/Printer.php
@@ -7,6 +7,11 @@ use \PhpParser\Node\Stmt;
 class Printer extends \EBT\ExtensionBuilder\Service\Printer {
 	protected $indentToken = "\t";
 
+	public function pStmt_Class(Stmt\Class_ $node) {
+		$string = parent::pStmt_Class($node);
+		return str_replace(LF . '{', ' {', $string);
+	}
+
 	public function pStmt_ClassMethod(Stmt\ClassMethod $node) {
 		$string = parent::pStmt_ClassMethod($node);
 		return str_replace(LF . '{', ' {', $string);
