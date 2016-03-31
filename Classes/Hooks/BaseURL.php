450069d369ffecd8ebcb7c9964c84d8fce14e7bc
Robert Vock <robert.vock@4wdmedia.de>
2016-03-31 09:34:26 +0200

    improved baseURL detection

--- Classes/Hooks/BaseURL.php
+++ Classes/Hooks/BaseURL.php
@@ -13,29 +13,13 @@ class BaseURL {
 			return;
 		}
 
-		$urlParts = parse_url($params['config']['baseURL']);
-		if (empty($urlParts['host'])) {
-			return;
-		}
-
-		$host = $urlParts['host'];
+		$host = preg_replace('/\.[^.]+$/', '', $_SERVER['SERVER_NAME']);
 		if (!is_link($_SERVER['4WD_CONFIG'] . '/domains/' . $host)) {
 			return;
 		}
 
 		// the current domain has a symlink with our configuration. Update the baseURL
 
-		if (substr($_SERVER['SERVER_NAME'], 0, strlen($host)) === $host) {
-			// just use the server name
-			$urlParts['host'] = $_SERVER['SERVER_NAME'];
-		} else {
-			$machineName = $_SERVER['USER'];
-			$urlParts['host'] = $host . '.' . $machineName;
-		}
-
-		// local development is always http
-		$urlParts['scheme'] = 'http';
-
-		$params['config']['baseURL'] = \TYPO3\CMS\Core\Utility\HttpUtility::buildUrl($urlParts);
+		$params['config']['baseURL'] = 'http://' . $_SERVER['SERVER_NAME'] . '/';
 	}
 }
