6971b6ebcc40f9f877e557a25cd0878a40146ef7
Robert Vock <robert.vock@4wdmedia.de>
2016-07-25 09:58:24 +0200

    Fixed Typo3Version for systems without data:-wrapper enabled

--- Classes/Controller/Typo3Version.php
+++ Classes/Controller/Typo3Version.php
@@ -5,7 +5,10 @@ namespace Vierwd\VierwdBase\Controller;
 class Typo3Version {
 
 	public function main(\Psr\Http\Message\RequestInterface $request, \TYPO3\CMS\Core\Http\Message $response) {
-		$stream = new \TYPO3\CMS\Core\Http\Stream(fopen('data:text/plain,' . TYPO3_version, 'r'));
+		$stream = fopen('php://memory','r+');
+		fwrite($stream, TYPO3_version);
+		rewind($stream);
+		$stream = new \TYPO3\CMS\Core\Http\Stream($stream);
 		$response = $response->withBody($stream);
 		$response = $response->withAddedHeader('Content-Type', 'text/plain');
 		$response = $response->withAddedHeader('Access-Control-Allow-Origin', '*');
