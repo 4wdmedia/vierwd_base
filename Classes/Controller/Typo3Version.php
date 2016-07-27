4f34a23039f3c9d68773aa3afdf3ef6982cdf3e5
Robert Vock <robert.vock@4wdmedia.de>
2016-07-27 16:46:08 +0200

    smaller changes

--- Classes/Controller/Typo3Version.php
+++ Classes/Controller/Typo3Version.php
@@ -5,7 +5,7 @@ namespace Vierwd\VierwdBase\Controller;
 class Typo3Version {
 
 	public function main(\Psr\Http\Message\RequestInterface $request, \TYPO3\CMS\Core\Http\Message $response) {
-		$stream = fopen('php://memory','r+');
+		$stream = fopen('php://memory', 'r+');
 		fwrite($stream, TYPO3_version);
 		rewind($stream);
 		$stream = new \TYPO3\CMS\Core\Http\Stream($stream);
