045195580cd10e498c77041b53ff19f62a321279
Robert Vock <robert.vock@4wdmedia.de>
2016-10-20 15:30:41 +0200

    Set jpeg quality and interlace also for images that have extra parameters

--- Classes/Imaging/GraphicalFunctions.php
+++ Classes/Imaging/GraphicalFunctions.php
@@ -14,7 +14,20 @@ class GraphicalFunctions extends \TYPO3\CMS\Core\Imaging\GraphicalFunctions {
 	}
 
 	public function imageMagickConvert($imagefile, $newExt = '', $w = '', $h = '', $params = '', $frame = '', $options = array(), $mustCreate = TRUE) {
-		// mustCreate has another default value
+		// Note: mustCreate has another default value
+
+		$ext = $newExt ?: strtolower(pathinfo($imagefile, PATHINFO_EXTENSION));
+		if ($params && in_array($ext, ['jpeg', 'jpg'])) {
+			// check if interlace plane and quality is set
+			if (strpos($params, '-quality') === false) {
+				$params .= ' -quality ' . $this->jpegQuality;
+			}
+
+			if (strpos($params, '-interlace') === false) {
+				$params .= ' -interlace Plane';
+			}
+		}
+
 		return parent::imageMagickConvert($imagefile, $newExt, $w, $h, $params, $frame, $options, $mustCreate);
 	}
 }
