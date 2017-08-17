858b97bc3c006f575705f6d0a44209a3f2a648cd
Robert Vock <robert.vock@4wdmedia.de>
2017-08-17 16:13:05 +0200

    added Canonical::getUrl method as getter for current URL (for social sharing)

--- Classes/Seo/Canonical.php
+++ Classes/Seo/Canonical.php
@@ -4,33 +4,51 @@ namespace Vierwd\VierwdBase\Seo;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class Canonical {
+class Canonical implements \TYPO3\CMS\Core\SingletonInterface {
 
 	public function getTag($content, array $params = []) {
 		if (!empty($GLOBALS['TSFE']->no_cache) || !empty($_SERVER['HTTP_X_PAGENOTFOUND'])) {
 			return '';
 		}
 
-		$url = $GLOBALS['TSFE']->id;
-		$parameters = '';
+		$url = self::getUrl();
+
+		$url = $GLOBALS['TSFE']->cObj->stdWrap($url, $params);
+
+		return $url;
+	}
+
+	static public function getUrl() {
+		if (!empty($GLOBALS['TSFE']->no_cache)) {
+			return '';
+		}
+
 		if ($GLOBALS['TSFE']->cHash) {
-			$GET = $_GET;
-			unset($GET['cHash'], $GET['id']);
-			$parameters = GeneralUtility::implodeArrayForUrl('', $GET);
+			$url = $GLOBALS['TSFE']->anchorPrefix;
+			$urlData = parse_url($url);
+			parse_str($urlData['query'], $query);
+			if (isset($query['tab']) || isset($query['stage'])) {
+				unset($query['tab'], $query['cHash']);
+				if (!$query) {
+					$url = $GLOBALS['TSFE']->id;
+				} else {
+					// recalc cHash
+					$url = $GLOBALS['TSFE']->cObj->typolink_url([
+						'useCacheHash' => true,
+						'additionalParams' => GeneralUtility::implodeArrayForUrl('', $query),
+					]);
+				}
+			}
+		} else {
+			$url = $GLOBALS['TSFE']->id;
 		}
 
 		if ($url) {
 			$conf = [
 				'parameter' => $url,
-				'additionalParams' => $parameters,
-				'useCacheHash' => true,
 				'forceAbsoluteUrl' => true,
-				'forceAbsoluteUrl.' => [
-					'scheme' => 'http' . (GeneralUtility::getIndpEnv('TYPO3_SSL') ? 's' : ''),
-				],
 			];
-			$url = $this->cObj->typolink_url($conf);
-			$url = $this->cObj->stdWrap($url, $params);
+			$url = $GLOBALS['TSFE']->cObj->typolink_url($conf);
 		}
 
 		return $url;
