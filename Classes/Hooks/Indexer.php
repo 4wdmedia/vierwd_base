6fcb51a2ca6c50f5ca057442eb7c1ce4dbd19146
Robert Vock <robert.vock@4wdmedia.de>
2018-06-04 17:15:52 +0200

    also index pages with language fallback

--- Classes/Hooks/Indexer.php
+++ Classes/Hooks/Indexer.php
@@ -13,9 +13,18 @@ class Indexer {
 			$pObj->content = str_replace($pObj->config['config']['spamProtectEmailAddresses_atSubst'], '@', $pObj->content);
 		}
 
+		$oldLanguageContent = $pObj->sys_language_content;
+		if ($pObj->sys_language_uid != $pObj->sys_language_content) {
+			// Current page shows fallback content of another language.
+			// Normally this would not be indexed. But if this is just another locale (de_CH and fallback to de_DE)
+			// we want this to be indexed
+			$pObj->sys_language_content = $pObj->sys_language_uid;
+		}
+
 		$_procObj = GeneralUtility::makeInstance(\TYPO3\CMS\IndexedSearch\Indexer::class);
 		$_procObj->hook_indexContent($pObj);
 
 		$pObj->content = $contentBefore;
+		$pObj->sys_language_content = $oldLanguageContent;
 	}
 }
