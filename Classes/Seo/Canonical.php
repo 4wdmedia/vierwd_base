10ea4514607f689d9196828a5ce8a4e9183075a5
Robert Vock <robert.vock@4wdmedia.de>
2018-08-10 16:41:36 +0200

    Improved canonical URL generation and added configuration for ignored variables

--- Classes/Seo/Canonical.php
+++ Classes/Seo/Canonical.php
@@ -24,20 +24,48 @@ class Canonical implements \TYPO3\CMS\Core\SingletonInterface {
 		}
 
 		if ($GLOBALS['TSFE']->cHash) {
-			$url = substr(GeneralUtility::getIndpEnv('TYPO3_REQUEST_URL'), strlen(GeneralUtility::getIndpEnv('TYPO3_SITE_URL')));
-			$urlData = parse_url($url);
-			parse_str($urlData['query'], $query);
-			if (isset($query['tab']) || isset($query['stage'])) {
-				unset($query['tab'], $query['cHash']);
+			$query = GeneralUtility::_GET();
+			$removeParameters = (array)$GLOBALS['TSFE']->config['config']['tx_vierwd.']['removeCanonicalUrlParameters.'];
+			$removeParameters = array_filter($removeParameters);
+			$queryChanged = false;
+			foreach ($removeParameters as $parameter) {
+				if (ArrayUtility::isValidPath($query, $parameter, '|')) {
+					$query = ArrayUtility::removeByPath($query, $parameter, '|');
+					$queryChanged = true;
+				}
+			}
+
+			$query2 = $query;
+			foreach ($query2 as $parameter => $value) {
+				if (is_array($value) && !$value) {
+					// empty array -> remove
+					unset($query[$parameter]);
+					$queryChanged = true;
+				}
+			}
+
+			if ($queryChanged) {
+				unset($query['cHash']);
+			}
+
+			if ($queryChanged) {
+				// regenerate URL
+
+				// First: remove L and id parameter
+				$query = array_diff_key($query, ['L' => 0, 'id' => 0]);
 				if (!$query) {
+					// there are more parameters beside L and id. Regenerate including cHash
 					$url = $GLOBALS['TSFE']->id;
 				} else {
-					// recalc cHash
+					// only L and id left. generate without cHash
 					$url = $GLOBALS['TSFE']->cObj->typolink_url([
+						'parameter' => $GLOBALS['TSFE']->id,
 						'useCacheHash' => true,
 						'additionalParams' => GeneralUtility::implodeArrayForUrl('', $query),
 					]);
 				}
+			} else {
+				$url = substr(GeneralUtility::getIndpEnv('TYPO3_REQUEST_URL'), strlen(GeneralUtility::getIndpEnv('TYPO3_SITE_URL')));
 			}
 		} else {
 			$url = $GLOBALS['TSFE']->id;
