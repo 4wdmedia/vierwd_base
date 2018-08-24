84427b3d96a83a0c89fb8f80ae5125878d7e7294
Robert Vock <robert.vock@4wdmedia.de>
2018-08-24 15:51:00 +0200

    added console commands for typo3-console (to replace custom php-scripts in database-folder)

---
+++ Classes/Console/Command/PostComposerCommandController.php
@@ -0,0 +1,59 @@
+<?php // PHP 7
+declare(strict_types=1);
+namespace Vierwd\VierwdBase\Console\Command;
+
+use Helhum\Typo3Console\Mvc\Cli\CommandDispatcher;
+use Helhum\Typo3Console\Mvc\Controller\CommandController;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class PostComposerCommandController extends CommandController {
+
+	/**
+	 * @var CommandDispatcher
+	 */
+	private $commandDispatcher;
+
+	public function __construct(CommandDispatcher $commandDispatcher = null) {
+		$this->commandDispatcher = $commandDispatcher ?: CommandDispatcher::createFromCommandRun();
+	}
+
+	/**
+	 * ensure some folders exist after composer installation
+	 */
+	public function runCommand() {
+		// create typo3temp, if it does not exist
+		if (!file_exists(PATH_site . 'typo3temp')) {
+			GeneralUtility::mkdir(PATH_site . 'typo3temp');
+			$this->outputLine('<info>Created typo3temp.</info>');
+		}
+
+		if (!empty($_SERVER['VIERWD_CONFIG'])) {
+			// create symlink to static resources, if the link does not exist and this is the dev-environment
+			$staticResources = PATH_site . 'static-resources';
+			if (!is_link($staticResources)) {
+				$composerFile = dirname(PATH_site) . '/composer.json';
+				$composerInfo = json_decode(file_get_contents($composerFile), true);
+				if ($composerInfo && isset($composerInfo['extra'], $composerInfo['extra']['extensionName'])) {
+					$target = 'typo3conf/ext/' . $composerInfo['extra']['extensionName'] . '/Resources/Public/static';
+					$target = escapeshellarg($target);
+					$staticResources = escapeshellarg($staticResources);
+					`ln -s $target $staticResources`;
+					$this->outputLine('<info>Added Link for static-resources.</info>');
+				}
+			}
+
+			// create AdditionalConfiguration.php if it does not exist
+			$additionalConfiguration = PATH_site . 'typo3conf/AdditionalConfiguration.php';
+			$sampleAdditionalConfiguration = PATH_site . 'typo3conf/AdditionalConfiguration.sample.php';
+			if (!file_exists($additionalConfiguration) && file_exists($sampleAdditionalConfiguration)) {
+				file_put_contents($additionalConfiguration, file_get_contents($sampleAdditionalConfiguration));
+				$this->outputLine('<info>Added AdditionalConfiguration.php.</info>');
+			}
+		}
+
+		$this->commandDispatcher->executeCommand('cache:flush');
+		$this->commandDispatcher->executeCommand('cache:flush', ['--force', '--files-only']);
+
+		$this->quit(0);
+	}
+}
---
+++ Configuration/Console/Commands.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+return [
+	'commands' => [
+		'vierwd:post-composer' => [
+			'vendor' => 'vierwd',
+			'controller' => Vierwd\VierwdBase\Console\Command\PostComposerCommandController::class,
+			'controllerCommandName' => 'run',
+			'runLevel' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_ESSENTIAL,
+		],
+		'vierwd:database:export' => [
+			'vendor' => 'vierwd',
+			'controller' => Vierwd\VierwdBase\Console\Command\DatabaseCommandController::class,
+			'controllerCommandName' => 'export',
+		],
+		'vierwd:database:import' => [
+			'vendor' => 'vierwd',
+			'controller' => Vierwd\VierwdBase\Console\Command\DatabaseCommandController::class,
+			'controllerCommandName' => 'import',
+			'runLevel' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_MINIMAL,
+		],
+		'vierwd:database:kb-import' => [
+			'vendor' => 'vierwd',
+			'controller' => Vierwd\VierwdBase\Console\Command\DatabaseCommandController::class,
+			'controllerCommandName' => 'kbImport',
+		],
+		'vierwd:database:last-change' => [
+			'vendor' => 'vierwd',
+			'controller' => Vierwd\VierwdBase\Console\Command\DatabaseCommandController::class,
+			'controllerCommandName' => 'lastChange',
+		],
+	],
+];
