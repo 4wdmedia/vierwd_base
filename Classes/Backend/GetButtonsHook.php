0010ca4daf73086187a34506572845d5b9478416
Robert Vock <robert.vock@4wdmedia.de>
2018-09-21 17:10:58 +0200

    re-added saveAndClose buttons

--- Classes/Backend/GetButtonsHook.php
+++ Classes/Backend/GetButtonsHook.php
@@ -2,6 +2,11 @@
 
 namespace Vierwd\VierwdBase\Backend;
 
+use TYPO3\CMS\Backend\Template\Components\Buttons\InputButton;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 /**
  * TYPO3 7 moved all save-actions into a drop-down. Only the default save action can be reached with one click.
  * The other save actions would need two clicks (SplitButton). We change the SplitButton to multiple buttons in a group.
@@ -11,8 +16,35 @@ class GetButtonsHook {
 	public function adjustSaveAndClose(array $params) {
 		$buttonBars = $params['buttons'];
 
-		// TODO: Add saveAndClose for TYPO3 9
+		// find the save button and replace it
+		foreach ($buttonBars['left'] as &$buttonGroup) {
+			foreach ($buttonGroup as $button) {
+				if ($button instanceof InputButton && $button->getName() === '_savedok') {
+					$saveAndClose = new InputButton;
+
+					$saveAndClose->setForm($button->getForm());
+					$saveAndClose->setIcon($this->getIconFactory()->getIcon('actions-document-save-close', Icon::SIZE_SMALL));
+					$saveAndClose->setName('_saveandclosedok');
+					$saveAndClose->setTitle($this->getLanguageService()->sL(
+						'LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:rm.saveCloseDoc'
+					));
+					$saveAndClose->setValue('1');
+					$saveAndClose->setShowLabelText(false);
+					$buttonGroup[] = $saveAndClose;
+					break 2;
+				}
+			}
+			unset($buttonGroup);
+		}
 
 		return $buttonBars;
+	}
+
+	protected function getIconFactory() {
+		return GeneralUtility::makeInstance(IconFactory::class);
+	}
+
+	protected function getLanguageService() {
+		return $GLOBALS['LANG'];
 	}
 }
