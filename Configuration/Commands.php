1ff1937dcda7549b6b163896fe538467421cee36
Robert Vock <robert.vock@4wdmedia.de>
2019-07-17 17:41:07 +0200

    Moved commands definition

---
+++ Configuration/Commands.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+return [
+	'vierwd:post-composer' => [
+		'vendor' => 'vierwd',
+		'controller' => Vierwd\VierwdBase\Console\Command\PostComposerCommandController::class,
+		'controllerCommandName' => 'run',
+		'runLevel' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_COMPILE,
+	],
+	'vierwd:database:export' => [
+		'vendor' => 'vierwd',
+		'controller' => Vierwd\VierwdBase\Console\Command\DatabaseCommandController::class,
+		'controllerCommandName' => 'export',
+		'runLevel' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_MINIMAL,
+	],
+	'vierwd:database:import' => [
+		'vendor' => 'vierwd',
+		'controller' => Vierwd\VierwdBase\Console\Command\DatabaseCommandController::class,
+		'controllerCommandName' => 'import',
+		'runLevel' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_MINIMAL,
+	],
+	'vierwd:database:kb-import' => [
+		'vendor' => 'vierwd',
+		'controller' => Vierwd\VierwdBase\Console\Command\DatabaseCommandController::class,
+		'controllerCommandName' => 'kbImport',
+	],
+	'vierwd:database:last-change' => [
+		'vendor' => 'vierwd',
+		'controller' => Vierwd\VierwdBase\Console\Command\DatabaseCommandController::class,
+		'controllerCommandName' => 'lastChange',
+	],
+	'vierwd:list-words' => [
+		'vendor' => 'vierwd',
+		'controller' => Vierwd\VierwdBase\Console\Command\ListWordsCommandController::class,
+		'controllerCommandName' => 'run',
+	],
+
+	'vierwd:rsync:down' => [
+		'vendor' => 'vierwd',
+		'controller' => Vierwd\VierwdBase\Console\Command\RsyncCommandController::class,
+		'controllerCommandName' => 'down',
+	],
+];
