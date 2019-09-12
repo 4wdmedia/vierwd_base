052473d1a403cfd5540e06dac5bb72d8caea68cb
Robert Vock <robert.vock@4wdmedia.de>
2019-09-12 13:30:20 +0200

    Rsync all file storages

--- Classes/Console/Command/RsyncCommandController.php
+++ Classes/Console/Command/RsyncCommandController.php
@@ -7,7 +7,9 @@ use Helhum\Typo3Console\Mvc\Controller\CommandController;
 use Symfony\Component\Process\Process;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Service\FlexFormService;
 
 class RsyncCommandController extends CommandController {
 
@@ -24,7 +26,20 @@ class RsyncCommandController extends CommandController {
 			$this->quit(1);
 		}
 
-		$command = array_filter([
+		if ($config['ssh']['serverPath'] === '~/kundenbereich/') {
+			// serverPath still has default value
+			$this->outputLine('<error>No ssh server path set in extension configuration</error>');
+			$this->quit(1);
+		}
+
+		$folders = $this->getRsyncFolders();
+		if (!$folders) {
+			$this->outputLine('<error>No folders configured for rsync</error>');
+			$this->quit(1);
+		}
+		$folders = $this->transformFolders($folders, $config['ssh']);
+
+		$command = array_filter(array_merge([
 			'rsync',
 			($dryRun ? '--dry-run' : ''),
 			'--exclude', '_processed_',
@@ -34,18 +49,66 @@ class RsyncCommandController extends CommandController {
 			'--recursive',
 			'--itemize-changes',
 			'--delete',
-			$config['ssh']['liveUser'] . '@' . $config['ssh']['liveHost'] . ':' . $config['ssh']['serverPath'] . 'public/fileadmin/',
-			Environment::getPublicPath() . '/fileadmin/',
-		]);
+			'--relative',
+		], $folders, [
+			Environment::getProjectPath(),
+		]));
 
 		$importProcess = new Process($command);
 		$importProcess->setTimeout(0.0);
 		$importProcess->inheritEnvironmentVariables();
 		$importProcess->run($this->buildStreamOutput());
 
-		if (!$dryRun) {
+		if ($importProcess->getExitCode()) {
+			$this->outputLine('<error>Import failed</error>');
+		} else if (!$dryRun) {
 			$this->outputLine('<info>Import complete</info>');
 		}
+	}
+
+	protected function getRsyncFolders(): array {
+		$queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('sys_file_storage');
+		$storages = $queryBuilder->select('configuration')
+			->from('sys_file_storage')
+			->andWhere('configuration LIKE "<%"')
+			->execute();
+
+		$flexFormService = GeneralUtility::makeInstance(FlexFormService::class);
+		$folders = [];
+		foreach ($storages as $storage) {
+			$configuration = $flexFormService->convertFlexFormContentToArray($storage['configuration']);
+			if (!$configuration || !isset($configuration['pathType']) || $configuration['pathType'] !== 'relative') {
+				continue;
+			}
+			$folders[] = $configuration['basePath'];
+		}
+
+		if (!$folders) {
+			// check if fileadmin exists and use that
+			if (is_dir(Environment::getPublicPath() . '/fileadmin/')) {
+				$folders[] = 'fileadmin/';
+			}
+		}
+
+		return $folders;
+	}
+
+	protected function transformFolders(array $folders, array $sshConfig): array {
+		$publicPrefix = trim(str_replace(Environment::getProjectPath(), '', Environment::getPublicPath()), '/') . '/';
+		if ($publicPrefix !== '/') {
+			$publicPrefix = '/' . $publicPrefix;
+		}
+		$serverPath = trim($sshConfig['serverPath'], '/') .'/';
+
+		$folders = array_map(function(string $path) use ($sshConfig, $serverPath, $publicPrefix) {
+			$path = $serverPath . '.' . $publicPrefix . $path;
+
+			// normalize path a bit (replace ../)
+			$path = preg_replace('/[^\/]+\/\.\.\//', '', $path);
+			return $sshConfig['liveUser'] . '@' . $sshConfig['liveHost'] . ':' . $path;
+		}, $folders);
+
+		return $folders;
 	}
 
 	/**
