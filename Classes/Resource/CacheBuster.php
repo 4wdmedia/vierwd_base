552f3d85025f66da6b87d09b0cfd49b1736787f4
Robert Vock <robert.vock@4wdmedia.de>
2020-01-30 09:19:06 +0100

    Allow disabling of prependAbsRefPrefixToPublicUrl in TYPO3 v7 projects

--- Classes/Resource/CacheBuster.php
+++ Classes/Resource/CacheBuster.php
@@ -39,7 +39,7 @@ class CacheBuster {
 			$params['publicUrl'] = $publicUrl;
 		}
 
-		if (TYPO3_version >= '8.0.0' || isset($GLOBALS['TSFE'], $GLOBALS['TSFE']->config, $GLOBALS['TSFE']->config['config'], $GLOBALS['TSFE']->config['config']['tx_vierwd.']['prependAbsRefPrefixToPublicUrl'])) {
+		if (TYPO3_version >= '8.0.0' || isset($GLOBALS['TSFE'], $GLOBALS['TSFE']->config, $GLOBALS['TSFE']->config['config'], $GLOBALS['TSFE']->config['config']['tx_vierwd.']['prependAbsRefPrefixToPublicUrl']) && !empty($GLOBALS['TSFE']->config['config']['tx_vierwd.']['prependAbsRefPrefixToPublicUrl'])) {
 			// Add absRefPrefix infront of URLs. We do not use this for TYPO3 7, because we're still using sourceSetCollection
 			// for some projects (picture-configuration.ts) and this would lead to duplicate prefixes.
 			// Projects running TYPO3 7 must handle these issues themselves. But most are still using baseURL and do not need
