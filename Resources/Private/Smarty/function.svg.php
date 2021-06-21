c3dfaf4f1c0d76e172f0f34ce57d95e4c6a26faf
Robert Vock <robert.vock@4wdmedia.de>
2021-06-21 09:03:11 +0200

    Added SVG smarty function from vierwd_smarty

---
+++ Resources/Private/Smarty/function.svg.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use Vierwd\VierwdBase\Frontend\ContentObject\ScalableVectorGraphicsContentObject;
+
+/**
+ * Usage: {svg width='36' height='36' src='EXT:vierwd_example/Resources/Public/static/img/font-awesome/chevron-right.svg'}
+ */
+function smarty_function_svg(array $params, Smarty_Internal_Template $smarty): string {
+	$cObj = GeneralUtility::makeInstance(ContentObjectRenderer::class);
+	$svgObject = GeneralUtility::makeInstance(ScalableVectorGraphicsContentObject::class, $cObj);
+	return $svgObject->render($params);
+}
