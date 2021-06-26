4e3b8392913fbff63c717f352af186822503c048
Robert Vock <robert.vock@4wdmedia.de>
2021-06-26 14:23:31 +0200

    Added testcase for GetButtonsHook

---
+++ Tests/Unit/Backend/GetButtonsHookTest.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types = 1);
+
+namespace Vierwd\VierwdBase\Tests\Unit\Backend\Unit\Backend;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use Prophecy\Argument;
+use TYPO3\CMS\Backend\Template\Components\Buttons\InputButton;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Localization\LanguageService;
+
+use Vierwd\VierwdBase\Backend\GetButtonsHook;
+
+class GetButtonsHookTest extends UnitTestCase {
+
+	/** @var GetButtonsHook */
+	private $subject;
+
+	/** @var Icon */
+	private $iconMock;
+
+	public function setUp(): void {
+		$iconMock = $this->prophesize(Icon::class);
+
+		$iconFactoryMock = $this->prophesize(IconFactory::class);
+		$iconFactoryMock->getIcon(Argument::any(), Argument::any())->willReturn($iconMock->reveal());
+		$this->iconMock = $iconMock->reveal();
+
+		$languageServiceMock = $this->prophesize(LanguageService::class);
+		$languageServiceMock->sL(Argument::any())->willReturn('title');
+
+		$this->subject = new GetButtonsHook($iconFactoryMock->reveal(), $languageServiceMock->reveal());
+	}
+
+	public function testAdjustSaveAndCloseWithoutLeftButtons(): void {
+		$arrayWithoutLeftButtons = ['right' => 'ignore'];
+		$this->assertEquals($arrayWithoutLeftButtons, $this->subject->adjustSaveAndClose(['buttons' => $arrayWithoutLeftButtons]));
+	}
+
+	public function testAdjustSaveAndCloseWithoutSavedOkButton(): void {
+		$saveButtonMock = $this->prophesize(InputButton::class);
+		$saveButtonMock->getName()->willReturn('_savebutton');
+		$buttons = [
+			'left' => [
+				'group' => [$saveButtonMock->reveal()],
+			],
+		];
+		$this->assertEquals($buttons, $this->subject->adjustSaveAndClose(['buttons' => $buttons]));
+	}
+
+	public function testAdjustSaveAndCloseWithSavedOkButton(): void {
+		$saveButtonMock = new InputButton();
+		$saveButtonMock->setName('_savedok');
+		$buttons = [
+			'left' => [
+				'group' => [
+					'button1' => $saveButtonMock,
+				],
+			],
+		];
+		$result = $this->subject->adjustSaveAndClose(['buttons' => $buttons]);
+		$lastButton = array_pop($result['left']['group']);
+		$this->assertEquals('_saveandclosedok', $lastButton->getName());
+		$this->assertEquals('1', $lastButton->getValue());
+		$this->assertEquals(false, $lastButton->getShowLabelText());
+		$this->assertEquals('title', $lastButton->getTitle());
+		$this->assertEquals($this->iconMock, $lastButton->getIcon());
+	}
+}
