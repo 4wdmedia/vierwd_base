8413106a8495f43d3eaacd2c8e0196c89a35e83d
Robert Vock <robert.vock@4wdmedia.de>
2021-09-17 16:24:37 +0200

    Fixed phpstan issues

--- Classes/Resource/DuplicateFiles.php
+++ Classes/Resource/DuplicateFiles.php
@@ -5,6 +5,7 @@ namespace Vierwd\VierwdBase\Resource;
 
 use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Messaging\FlashMessageService;
+use TYPO3\CMS\Core\Resource\AbstractFile;
 use TYPO3\CMS\Core\Resource\Event\AfterFileAddedEvent;
 use TYPO3\CMS\Core\Resource\Index\FileIndexRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -17,8 +18,7 @@ class DuplicateFiles {
 	public function __invoke(AfterFileAddedEvent $event): void {
 		$file = $event->getFile();
 
-		// AbstractFile $file, Folder $targetFolder
-		if (!$file->getSize() || !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_BE) || (TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_CLI)) {
+		if (!($file instanceof AbstractFile) || !$file->getSize() || !(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_BE) || (TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_CLI)) {
 			// do not check for existence of empty files
 			return;
 		}
