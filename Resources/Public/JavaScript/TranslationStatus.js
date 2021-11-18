f0df6ed8a777f37be63d0a98c4601c02dfe54211
Robert Vock <robert.vock@4wdmedia.de>
2021-11-18 09:19:08 +0100

    Added simple search for translation-status

--- Resources/Public/JavaScript/TranslationStatus.js
+++ Resources/Public/JavaScript/TranslationStatus.js
@@ -1,5 +1,9 @@
 define(['jquery'], function($) {
 	var $table = $('.translation-status');
+	var $rows = $table.find('tbody').children();
+	var searchData = $.map($rows, function(row) {
+		return row.innerText.toLowerCase();
+	});
 
 	$(document.body).on('input', '.translation-status__checkbox input', function(event) {
 		if (this.checked) {
@@ -8,4 +12,23 @@ define(['jquery'], function($) {
 			$table.find('tr > *:nth-child(' + this.value + ')').attr('hidden', 'hidden');
 		}
 	});
+
+	var currentSearch = '';
+	$(document.body).on('input', '.translation-status__filter input', function(event) {
+		var search = this.value.toLowerCase();
+		if (search !== currentSearch) {
+			performSearch(search);
+		}
+	});
+
+	function performSearch(search) {
+		currentSearch = search;
+		$rows.attr('hidden', function(index) {
+			if (searchData[index].indexOf(search) !== -1) {
+				return false;
+			}
+
+			return 'hidden';
+		});
+	}
 });
