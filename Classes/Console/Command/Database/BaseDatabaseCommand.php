9d0f4de17f0334d225b403eb4fb97803782abd31
Robert Vock <robert.vock@4wdmedia.de>
2023-01-30 15:15:56 +0100

    Added `--content-only` option for kb-import command

--- Classes/Console/Command/Database/BaseDatabaseCommand.php
+++ Classes/Console/Command/Database/BaseDatabaseCommand.php
@@ -68,7 +68,7 @@ abstract class BaseDatabaseCommand extends Command {
 	/**
 	 * get the command line to export all tables containing export worthy data
 	 */
-	protected function getExportDataTablesCommand(string $type = self::CONNECTION_LOCAL): string {
+	protected function getExportDataTablesCommand(string $type = self::CONNECTION_LOCAL, bool $contentOnly = false): string {
 		$additionalArguments = [
 			'--skip-lock-tables',
 			'--default-character-set=utf8mb4',
@@ -84,8 +84,16 @@ abstract class BaseDatabaseCommand extends Command {
 			$dbConfig = $configurationManager->getConfigurationValueByPath('DB/Connections/Default');
 		}
 
-		foreach ($this->getIgnoredTables() as $table) {
-			$additionalArguments[] = sprintf('--ignore-table=%s.%s', $dbConfig['dbname'], $table);
+		assert(is_array($dbConfig));
+
+		if ($contentOnly) {
+			$additionalArguments[] = 'pages';
+			$additionalArguments[] = 'sys_file_reference';
+			$additionalArguments[] = 'tt_content';
+		} else {
+			foreach ($this->getIgnoredTables() as $table) {
+				$additionalArguments[] = sprintf('--ignore-table=%s.%s', $dbConfig['dbname'], $table);
+			}
 		}
 
 		$connectionArguments = $type === self::CONNECTION_LOCAL ? $this->buildConnectionArguments() : $this->buildRemoteConnectionArguments();
