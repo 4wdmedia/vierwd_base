046b5481e557ddf15d1336a3c13d44a377714f57
Robert Vock <robert.vock@4wdmedia.de>
2024-02-01 13:11:18 +0100

    Removed usage of TYPO3_REQUESTTYPE

--- phpstan.bootstrap.php
+++ phpstan.bootstrap.php
@@ -9,13 +9,6 @@ defined('CRLF') ?: define('CRLF', CR . LF);
 
 defined('FILE_DENY_PATTERN_DEFAULT') ?: define('FILE_DENY_PATTERN_DEFAULT', '\\.(php[3-8]?|phpsh|phtml|pht|phar|shtml|cgi)(\\..*)?$|\\.pl$|^\\.htaccess$');
 
-defined('TYPO3_REQUESTTYPE') ?: define('TYPO3_REQUESTTYPE', 0);
-defined('TYPO3_REQUESTTYPE_FE') ?: define('TYPO3_REQUESTTYPE_FE', 1);
-defined('TYPO3_REQUESTTYPE_BE') ?: define('TYPO3_REQUESTTYPE_BE', 2);
-defined('TYPO3_REQUESTTYPE_CLI') ?: define('TYPO3_REQUESTTYPE_CLI', 4);
-defined('TYPO3_REQUESTTYPE_AJAX') ?: define('TYPO3_REQUESTTYPE_AJAX', 8);
-defined('TYPO3_REQUESTTYPE_INSTALL') ?: define('TYPO3_REQUESTTYPE_INSTALL', 16);
-
 defined('TYPO3_MODE') ?: define('TYPO3_MODE', '');
 defined('TYPO3_mainDir') ?: define('TYPO3_mainDir', 'typo3/');
 defined('TYPO3_version') ?: define('TYPO3_version', '');
